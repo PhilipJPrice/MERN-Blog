7f18abd4752c94d5342a5d1d5b1be50c919bf338
PhilipJPrice <philip.price.1994@gmail.com>
2025-09-08 10:07:17 -0700

    build: blog build with CI/CD workflows

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,7 +1,5 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import prettierConfig from 'eslint-config-prettier'
 
@@ -10,9 +8,7 @@ export default defineConfig([
   {
     files: ['**/*.{js,jsx}'],
     extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      js.configs.recommended
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -27,8 +23,7 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-			'react/prop-types': 'off',
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }]
     },
   },
 	prettierConfig,
